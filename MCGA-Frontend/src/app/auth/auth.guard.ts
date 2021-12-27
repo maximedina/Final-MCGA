edfd3525afa46e20cfe12d864d1485ae83ef689b
Medina <maximiliano.medina@arcelormittal.com.ar>
2021-12-27 14:36:21 -0300

    Frontend 1er commit

---
+++ MCGA-Frontend/src/app/auth/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SecurityService } from '../services/security.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  
+  constructor(
+      private router: Router,
+      private securityService: SecurityService
+  ) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    let permission = next.data.permission;
+
+    if (localStorage.getItem('token') !== null && localStorage.getItem('user') !== null) {
+      return true;
+    } else {
+      console.log('redireccion auth.guard');
+      this.securityService.logout();
+      //this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
