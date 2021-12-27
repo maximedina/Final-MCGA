d1058da57d345a71cdb9c7a781fd55bb9df3090c
Medina <maximiliano.medina@arcelormittal.com.ar>
2021-12-27 11:48:47 -0300

    Commit Backend

---
+++ MCGA-Backend/coreWebAPI/coreWebAPI/Db/BaseDbContext.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MOM.Core.Db
+{
+    public abstract class BaseDbContext : DbContext
+    {
+        public BaseDbContext(DbContextOptions options) : base(options) { }
+
+        public T AddOrUpdate<T>(T entity) where T : class
+        {
+            if (Entry(entity).State == EntityState.Detached)
+            {
+                Attach(entity);
+            }
+            if(Entry(entity).State != EntityState.Added)
+            {
+                Set<T>().Update(entity);
+            }
+            SaveChanges();
+            return entity;
+        }
+
+        public bool TryDeleteEntity<T>(T entity) where T : class
+        {
+            try
+            {
+                Set<T>().Remove(entity);
+                SaveChanges();
+                return true;
+            }
+            catch (Exception){ return false; }            
+        }
+
+        private bool Exists<T>(T entity) where T : class
+        {
+            return Set<T>().Local.Any(e => e == entity);
+        }
+
+        private IEnumerable<string> GetIncludePaths(Type clrEntityType)
+        {
+            var entityType = Model.FindEntityType(clrEntityType);
+            var includedNavigations = new HashSet<INavigation>();
+            var stack = new Stack<IEnumerator<INavigation>>();
+
+            while (true)
+            {
+                var entityNavigations = new List<INavigation>();
+                foreach (var navigation in entityType.GetNavigations())
+                {
+                    if (includedNavigations.Add(navigation) || stack.Count > 1)
+                        entityNavigations.Add(navigation);
+                }
+                if (entityNavigations.Count == 0)
+                {
+                    if (stack.Count > 0)
+                        yield return string.Join(".", stack.Reverse().Select(e => e.Current.Name));
+                }
+                else
+                {
+                    foreach (var navigation in entityNavigations)
+                    {
+                        var inverseNavigation = navigation.FindInverse();
+                        if (inverseNavigation != null)
+                            includedNavigations.Add(inverseNavigation);
+                    }
+                    stack.Push(entityNavigations.GetEnumerator());
+                }
+                while (stack.Count > 0 && !stack.Peek().MoveNext())
+                    stack.Pop();
+                if (stack.Count == 0) break;
+                entityType = stack.Peek().Current.GetTargetType();
+            }
+        }
+
+		public List<TEntity> GetAll<TEntity>() where TEntity : class 
+		{
+			return Set<TEntity>().ToList();
+		}
+	}
+}
---
+++ MCGA-Backend/coreWebAPI/coreWebAPI/Db/CoreDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MOM.Core.Models.Security;
+using MOM.Core.Db.Configuration.Security;
+using MOM.Core.Models.Localizacion;
+using MOM.Core.Db.Configuration.Localizacion;
+
+namespace MOM.Core.Db
+{
+    public class CoreDbContext : BaseDbContext
+    {
+        #region Tables
+
+        #region Localizacion
+        public DbSet<Provincia> Provincias { get; set; }
+        public DbSet<Ciudad> Ciudades { get; set; }
+        #endregion
+
+        #region Security
+        public DbSet<SystemModule> SystemModules { get; set; }
+        public DbSet<SystemSetting> SystemSettings { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ProfilePermission> ProfilePermissions { get; set; }
+        public DbSet<OperationLog> OperationLog { get; set; }
+        public DbSet<Session> Session { get; set; }
+        public DbSet<SessionLog> SessionLog { get; set; }
+
+        #endregion
+
+        #endregion
+
+        public CoreDbContext(DbContextOptions<CoreDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region Database Properties
+
+            #region Localizacion
+            modelBuilder.ApplyConfiguration(new ProvinciasConfiguration());
+            modelBuilder.ApplyConfiguration(new CiudadesConfiguration());
+            #endregion
+
+            #region security
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new PermissionConfiguration());
+            modelBuilder.ApplyConfiguration(new ProfileConfiguration());
+            modelBuilder.ApplyConfiguration(new ProfilePermissionConfiguration());
+            modelBuilder.ApplyConfiguration(new SystemModuleConfiguration());
+            modelBuilder.ApplyConfiguration(new SystemSettingConfiguration());
+            modelBuilder.ApplyConfiguration(new OperationLogConfiguration());
+            modelBuilder.ApplyConfiguration(new SessionConfiguration());
+            #endregion
+
+            #endregion
+        }
+    }
+}
